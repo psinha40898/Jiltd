e1f46ae86bb25ef6cefa68a954767717854678b6
pyush <pyushsinha20@gmail.com>
2024-01-03 22:12:27 -0800

    1/3/2024 dirty dancing in da moonlight

--- screens/matchMake.ts
+++ screens/matchMake.ts
@@ -14,88 +14,83 @@ navigation: NativeStackNavigationProp<RootStackParamList>
     if (delta > 12000){
       break;
     }
-  const clientUserDocRef: DocumentReference<DocumentData, DocumentData> = doc(db,'queue',clientUserID);
-  const clientUserDocSnap: DocumentSnapshot<DocumentData, DocumentData> = await getDoc(clientUserDocRef);
-  if (!clientUserDocSnap.exists()){
-    await setDoc(clientUserDocRef, {
-      matchedID: "Open"
-    })
-  }
- 
+    const clientUserDocRef: DocumentReference<DocumentData, DocumentData> = doc(db,'queue',clientUserID);
+    const clientUserDocSnap: DocumentSnapshot<DocumentData, DocumentData> = await getDoc(clientUserDocRef);
+    if (!clientUserDocSnap.exists()){
+      await setDoc(clientUserDocRef, {
+        matchedID: "Open"
+      })
+    }
   
-  let matchedUser: QueryDocumentSnapshot<DocumentData, DocumentData>; 
-  try 
-  {
-    finalMatchID = await runTransaction(db, async (transaction) => {
-      const matchMakingPoolRef: CollectionReference<DocumentData, DocumentData> = collection(db, "queue");
-      const matchMakingPoolSnap: QuerySnapshot<DocumentData, DocumentData> = await getDocs(matchMakingPoolRef);
-      const clientUserDocSnap = await transaction.get(clientUserDocRef);
-      const clientUserMatchedID = clientUserDocSnap.data()?.matchedID ?? "Open";
-      if (clientUserMatchedID !== "Open"){
-        inQueue = false;
-        return clientUserMatchedID;
-      }
-
-      for (const doc of matchMakingPoolSnap.docs) 
-      {
-        const curUser = await transaction.get(doc.ref); 
-        if (!curUser.exists()) 
-        {
-          throw "Document does not exist!";
+    
+    let matchedUser: QueryDocumentSnapshot<DocumentData, DocumentData>; 
+    try 
+    {
+      finalMatchID = await runTransaction(db, async (transaction) => {
+        const matchMakingPoolRef: CollectionReference<DocumentData, DocumentData> = collection(db, "queue");
+        const matchMakingPoolSnap: QuerySnapshot<DocumentData, DocumentData> = await getDocs(matchMakingPoolRef);
+        const clientUserDocSnap = await transaction.get(clientUserDocRef);
+        const clientUserMatchedID = clientUserDocSnap.data()?.matchedID ?? "Open";
+        if (clientUserMatchedID !== "Open"){
+          inQueue = false;
+          return clientUserMatchedID;
         }
-        console.log(curUser.id)
-        if (curUser.id !== clientUserID && curUser.exists()) 
-        { 
-          matchedUser = doc
-          if (matchedUser.id !== "emptyQ"){
+        for (const doc of matchMakingPoolSnap.docs) 
+        {
+          const curUser = await transaction.get(doc.ref); 
+          if (!curUser.exists()) 
+          {
+            throw "Document does not exist!";
+          }
+          console.log(curUser.id)
+          if (curUser.exists() && curUser.id !== clientUserID && curUser.id !== "emptyQ") 
+          { 
+            matchedUser = curUser;
             break;
           }
         }
-      }
-      if (matchedUser.id === "emptyQ")
+        if (!matchedUser)
+        {
+          inQueue = true;
+          throw "Queue is empty! Nobody's home."
+        }
+
+        const writetoMatch = clientUserID;
+        transaction.update(matchedUser.ref, { matchedID: writetoMatch});
+        const matchedUserID = matchedUser.id
+        return matchedUserID
+      });
+      } 
+    catch (e) 
+    {
+      console.log("Failed", e);
+    }
+
+    try
+    {
+      if (typeof finalMatchID === 'undefined')
       {
-        inQueue = true;
-        throw "Queue is empty! Nobody's home."
+        throw "Undefined!";
       }
-
-      const writetoMatch = clientUserID;
-      transaction.update(matchedUser.ref, { matchedID: writetoMatch});
-      const matchedUserID = matchedUser.id
-      return matchedUserID
-    });
-    } 
-  catch (e) 
-  {
-    console.log("Failed", e);
-  }
-
-  try{
-    if (typeof finalMatchID === 'undefined')
-    {
-      throw "queue again";
+      if (finalMatchID === "")
+      {
+        throw "Blank!";
+      }
+      inQueue = false;
+      const clientUserDocRefCheck: DocumentReference<DocumentData, DocumentData> = doc(db,'queue',clientUserID);
+      const clientUserDocSnapCheck: DocumentSnapshot<DocumentData, DocumentData> = await getDoc(clientUserDocRefCheck);
+      if (clientUserDocSnapCheck.exists())
+      {
+        await deleteDoc(clientUserDocRefCheck);
+        console.log("DELETE");
+      }
+      console.log(finalMatchID, clientUserID)
+      navigation.navigate("MatchScreen", { match: finalMatchID, self: clientUserID})
     }
-    if (finalMatchID === "")
+    catch(e)
     {
-
-      throw "empty Q";
+      console.log(e);
     }
-    inQueue = false;
-    const clientUserDocRefCheck: DocumentReference<DocumentData, DocumentData> = doc(db,'queue',clientUserID);
-    const clientUserDocSnapCheck: DocumentSnapshot<DocumentData, DocumentData> = await getDoc(clientUserDocRefCheck);
-    if (clientUserDocSnapCheck.exists())
-    {
-      await deleteDoc(clientUserDocRefCheck);
-      console.log("DELETE");
-    }
-    console.log(finalMatchID, clientUserID)
-    navigation.navigate("MatchScreen", { match: finalMatchID, self: clientUserID})
-  }
-  catch(e)
-  {
-    console.log(e);
-  }
-
-  const clientUserDocRefCheck: DocumentReference<DocumentData, DocumentData> = doc(db,'queue',clientUserID);
   }
   }
 
