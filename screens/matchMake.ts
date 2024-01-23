6711bf4aef14139ce86f21dacc85ba99e5538835
pyush <pyushsinha20@gmail.com>
2024-01-23 01:47:44 -0800

    Rectifying ACID based matchmaking matchMake.ts

--- screens/matchMake.ts
+++ screens/matchMake.ts
@@ -25,7 +25,7 @@ navigation: NativeStackNavigationProp<RootStackParamList>
     }
   
     
-    let matchedUser: QueryDocumentSnapshot<DocumentData, DocumentData>; 
+    let matchedUser;
     try 
     {
       finalMatchID = await runTransaction(db, async (transaction) => {
@@ -33,9 +33,8 @@ navigation: NativeStackNavigationProp<RootStackParamList>
         const matchMakingPoolSnap: QuerySnapshot<DocumentData, DocumentData> = await getDocs(matchMakingPoolRef);
         const clientUserDocSnap = await transaction.get(clientUserDocRef);
         const clientUserMatchedID = clientUserDocSnap.data()?.matchedID ?? "Open";
-        if (clientUserMatchedID !== "Open"){
+        if (clientUserMatchedID !== "Open"){ //the error im trying to debug occurs when chosen by someone
           console.log("Chosen by someone")
-          inQueue = false;
           return clientUserMatchedID;
         }
         for (const doc of matchMakingPoolSnap.docs) 
@@ -46,7 +45,7 @@ navigation: NativeStackNavigationProp<RootStackParamList>
             throw "Document does not exist!";
           }
           console.log(curUser.id)
-          if (curUser.exists() && curUser.id !== clientUserID && curUser.id !== "emptyQ") 
+          if (curUser.exists() && curUser.id !== clientUserID && curUser.id !== "emptyQ" && (curUser.data().matchedID === "Open" || curUser.data().matchedID === clientUserID)) 
           { 
             matchedUser = curUser;
             break;
@@ -59,7 +58,11 @@ navigation: NativeStackNavigationProp<RootStackParamList>
         }
 
         const writetoMatch = clientUserID;
-        transaction.update(matchedUser.ref, { matchedID: writetoMatch});
+        //if open
+        if (matchedUser.data().matchedID === "Open"){
+          transaction.update(matchedUser.ref, { matchedID: writetoMatch});console.log("error is here")
+          transaction.update(clientUserDocRef, { matchedID: matchedUser.id})
+        }
         const matchedUserID = matchedUser.id
         return matchedUserID
       });
@@ -79,17 +82,24 @@ navigation: NativeStackNavigationProp<RootStackParamList>
       {
         throw "Can't find a single valid match!";
       }
-      inQueue = false;
+
       const clientUserDocRefCheck: DocumentReference<DocumentData, DocumentData> = doc(db,'queue',clientUserID);
       const clientUserDocSnapCheck: DocumentSnapshot<DocumentData, DocumentData> = await getDoc(clientUserDocRefCheck);
-      if (clientUserDocSnapCheck.exists())
-      {
-        await deleteDoc(clientUserDocRefCheck);
-        console.log("LEFT QUEUE");
-      }
+      const matchUserDocRefCheck: DocumentReference<DocumentData, DocumentData> = doc(db,'queue',finalMatchID);
+      const matchUserDocSnapCheck: DocumentSnapshot<DocumentData, DocumentData> = await getDoc(matchUserDocRefCheck);
+      //scenario 1 -- someone wrote to you, and you didnt write to anybody finalMatch holds writer's valid ID
+      //scenario 2 -- you wrote to somebody finalmatch holds their valid ID holds writee's ID
+      //scenario 3 -- you didnt write to anybody, and nobody wrote to you (this is the case where you're matching with someone invalid)
+
       console.log(finalMatchID, clientUserID)
-      navigation.navigate("MatchScreen", { match: finalMatchID, self: clientUserID})
-      break;
+      //we can put a check here to verify the signatures
+      //it's all about the signatures at this point and loop control.
+      if (clientUserDocSnapCheck.data().matchedID === finalMatchID && matchUserDocSnapCheck.data().matchedID === clientUserID)
+      {
+        navigation.navigate("MatchScreen", { match: finalMatchID, self: clientUserID})
+        inQueue = false;
+      }
+      
     }
     catch(e)
     {
@@ -102,10 +112,20 @@ navigation: NativeStackNavigationProp<RootStackParamList>
   }
   const clientUserDocRefCheck: DocumentReference<DocumentData, DocumentData> = doc(db,'queue',clientUserID);
   const clientUserDocSnapCheck: DocumentSnapshot<DocumentData, DocumentData> = await getDoc(clientUserDocRefCheck);
-  if (clientUserDocSnapCheck.exists())
-  {
-    await deleteDoc(clientUserDocRefCheck); //deleting from the queue works -- but it throws a warning (not erorr) when a prospective match deletes
-    console.log("LEFT QUEUE");              //can fix by leaving people in queue -- making that the "online pool" and seperating by fields? less efficient.
-  }
+  // if (clientUserDocSnapCheck.exists())
+  // {
+  //   await deleteDoc(clientUserDocRefCheck); //deleting from the queue works -- but it throws a warning (not erorr) when a prospective match deletes
+  //   console.log("LEFT QUEUE");              //can fix by leaving people in queue -- making that the "online pool" and seperating by fields? less efficient.
+  // }
+  // console.log("final execution") // see  if this executes
+  // i can delete the document once we're in the chat
+  // then, instead of deleting it here, simmply edit a field
+  // make that field qualifying to a good match
+
+
+  //mark as "active" after creating doc
+  //the update is regular
+  //mark inactive before proceeding
+  //delete once in chat?
   }
 
