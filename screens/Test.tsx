23d79efe595ae7785abddb76ceb60b86e797f62c
pyush <pyushsinha20@gmail.com>
2024-01-26 12:25:43 -0800

    Started frontend for inventory management, map of items in user/userid document, todo: parse keys of count >0 into dropdown, style dropdown

--- screens/Test.tsx
+++ screens/Test.tsx
@@ -9,22 +9,31 @@ import IconButton from '../essentialComponents/Icon';
 import LoopAnimation from '../essentialComponents/LoopAnimation';
 import { matchMake } from './matchMake';
 import { auth } from '../firebase';
+import SelectDropdown from 'react-native-select-dropdown'
 
 
 const Test = () => {
   const userID = auth.currentUser.uid;
   const navigation = useNavigation<NativeStackNavigationProp<RootStackParamList>>();
-  const [modalVisible, setModalVisible] = useState(false);;
+  const [modalVisible, setModalVisible] = useState(false);
+  //need to load this array from the database
+  //this should load from users/userid.data().inventory
+  //either all the keys or the keys of 1 or more count
+  //parse the keys that have 1 or more into an array! **
+  //key(count), starterA(1), heartStopper(5)
+  const countries = ["fetch1", "fetch2", "fetch3"];
+  
   const talkButton = async () => {
     console.log("THIS IS THE USER ID", userID);
     setModalVisible(true);
-    //start loading screen
-    //state = true
     await matchMake(userID, navigation);
     setModalVisible(false);
-    //end loading screen
-    //state = false
   };
+
+  //**onSelect will set activeimg to doc*/
+  //The dropdown changes the activeimg value of the doc
+  //The options from the dropdown are dependent on the user's inventory
+  //
   
     return(
 <View style={[ tStyle.container,
@@ -36,18 +45,36 @@ const Test = () => {
 <View style={[{flex:1, flexDirection: 'row', justifyContent:'center', alignItems: 'center', marginTop: 5}, styles.primaryBGoffBlack]}>
   <Text style={[styles.primaryRed, styles.size2]}>hi.</Text></View>
 <View style={[{flex:1, flexDirection: 'row', justifyContent:'center', alignContent: 'center', alignItems: 'center', margin: 10}, styles.secondaryBGoffBlack]}>
-  <View style = {{flexDirection: 'column'}}><Text style ={[styles.size4, styles.primaryRed, {fontWeight:'600'}]}>smiley heartstopper</Text><Text style ={[styles.size4, styles.primaryRed]}>level 1</Text><Text style ={[styles.size4, styles.primaryRed]}>5 reps</Text><Text style ={[styles.size4, styles.primaryRed]}>5 negs</Text><Text style ={[styles.size4, styles.primaryRed]}>avatarName</Text></View>
+  <View style = {{flexDirection: 'column'}}><Text style ={[styles.size4, styles.primaryRed, {fontWeight:'600'}]}>fetch name</Text><Text style ={[styles.size4, styles.primaryRed]}>level 1</Text><Text style ={[styles.size4, styles.primaryRed]}>5 reps</Text><Text style ={[styles.size4, styles.primaryRed]}>5 negs</Text><Text style ={[styles.size4, styles.primaryRed]}>avatarName</Text>
+  <SelectDropdown
+	data={countries}
+  defaultButtonText='change icon'
+	onSelect={(selectedItem, index) => {
+		console.log(selectedItem, index)
+	}}
+	buttonTextAfterSelection={(selectedItem, index) => {
+		// text represented after item is selected
+		// if data array is an array of objects then return selectedItem.property to render after item is selected
+		return selectedItem
+	}}
+	rowTextForSelection={(item, index) => {
+		// text represented for each item in dropdown
+		// if data array is an array of objects then return item.property to represent item in dropdown
+		return item
+	}}
+/>
+  </View>
   <LoopAnimation
   onPress={() => console.log("Sorry")}
   imageComponent={<Image source={require('../images/heart-face-flatline.png')} style={{ width: 150, height: 150 }} />}
 /></View>
-<View style = {[{flex:0.5, alignItems: 'center', justifyContent: 'center', alignContent: 'center'}, styles.primaryBGoffBlack]}>          <Modal
-        animationType="slide"
+<View style = {[{flex:0.5, alignItems: 'center', justifyContent: 'center', alignContent: 'center'}, styles.primaryBGoffBlack]}>          
+<Modal  animationType="slide"
         transparent={true}
         visible={modalVisible}
         onRequestClose={() => {
-          Alert.alert('Modal has been closed.');
-          setModalVisible(!modalVisible);
+          Alert.alert('Modal has been closed.')
+          setModalVisible(!modalVisible)
         }}>
         <View style={styles.centeredView}>
           <View style={styles.modalView}>
@@ -55,8 +82,9 @@ const Test = () => {
           </View>
         </View>
       </Modal><FlashButton pressFunc = {talkButton} text={"Play"} ></FlashButton></View>
-<View style={[{flex:1}, styles.primaryBGoffBlack, {alignItems: 'center', justifyContent: 'center', alignContent: 'center'}]}><Text style ={[styles.primaryRed, styles.size4]}>what kind of idea are you? </Text></View>
-<View style={[{flex:1}, {backgroundColor: "#1c1c1c", borderBottomLeftRadius: 50, borderBottomRightRadius: 50} ]}><Text style ={[styles.primaryRed, styles.size4]}>test. </Text></View>
+<View style={[{flex:1}, styles.primaryBGoffBlack, {alignItems: 'center', justifyContent: 'center', alignContent: 'center'}]}><Text style ={[styles.primaryRed, styles.size4]}>fetch note </Text></View>
+<View style={[{flex:1}, {backgroundColor: "#1c1c1c", borderBottomLeftRadius: 50, borderBottomRightRadius: 50} ]}><Text style ={[styles.primaryRed, styles.size4]}>test. </Text>
+</View>
         <View style={[{flex:0.25}, styles.primaryBGBlack]}> 
         <View style={[{flexDirection:'row'}, { justifyContent: 'center', alignItems: 'center'}]}>
         <IconButton onPress={()=> console.log("Sorry")}></IconButton>
