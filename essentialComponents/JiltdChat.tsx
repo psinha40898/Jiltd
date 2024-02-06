abcfbac082a2f116c612a26d166919694f273151
pyush sinha <pyushsinha20@gmail.com>
2024-02-05 21:39:45 -0800

    JiltdChat.tsx

--- essentialComponents/JiltdChat.tsx
+++ essentialComponents/JiltdChat.tsx
@@ -1,12 +1,17 @@
-import {View, FlatList, Text } from "react-native"
+import {View, FlatList, Text, TextInput, KeyboardAvoidingView } from "react-native"
 import {doc, collection, db, getDoc, query, onSnapshot, orderBy, addDoc, Timestamp, setDoc} from '../firebase'
 import { useState, useEffect} from "react";
 import FlashButton from "./FlashButton";
+import styles from "./Style";
+import CustomKeyboardWrapper from "../conditionalComponents/CustomKeyboardWrapper";
 // <View style={item.sender ? styles.left : styles.right}>
 // item.sender === CONSTANT_STRING ? styles.left : styles.right or item.ID
+//system or match_ID is on left and an offshade of the background black
+//client is on right and jiltd red
 const JiltdChat = ({ client_ID, match_ID }) => {
+  //var updatedMessages = []
   var updatedMessages = []
-  var unsubscribe
+  const [sampleSend, setSample] = useState('');
   const smallerUserId = client_ID < match_ID ? client_ID : match_ID;
   const largerUserId = client_ID < match_ID ? match_ID : client_ID;
   const chatroomDocRef = doc(db, 'chatrooms', `${smallerUserId}_${largerUserId}`);
@@ -16,14 +21,27 @@ const JiltdChat = ({ client_ID, match_ID }) => {
     const sampleData = [{message: "Hey", sender : true, key: "unique1"}, {message: "Hey back", sender: false, key: "unique2"}]
     const sendMessage = async () => {
       await addDoc(messagesRef, {
-        text: 'XXXXXXXXXXXXXXX',
+        text: 'beezlebub',
         timestamp: Timestamp.now(),
         senderId: 'system',
         millisecond: Timestamp.now().toMillis()
       });
-      console.log(messages);
+
+    }
+    const testInput = async () => {
+
+      await addDoc(messagesRef, {
+        text: sampleSend,
+        timestamp: Timestamp.now(),
+        senderId: 'system',
+        millisecond: Timestamp.now().toMillis()
+      });
+
     }
 
+    useEffect(() => {
+      console.log("Updated messages:", messages);
+    }, [messages]);
     useEffect(() => {
       async function initializeChat() {
         try {
@@ -35,26 +53,14 @@ const JiltdChat = ({ client_ID, match_ID }) => {
               senderId: 'system',
               millisecond: Timestamp.now().toMillis()
             });
-          } else {
-            console.log("Executing");
-            await addDoc(messagesRef, {
-              text: 'Test',
-              timestamp: Timestamp.now(),
-              senderId: 'system',
-              millisecond: Timestamp.now().toMillis()
-            });
           }
     
-          const q = query(messagesRef, orderBy("millisecond"));
-          console.log(q);
+          const q = query(messagesRef, orderBy("millisecond", "desc"));
           const unsubscribe = onSnapshot(q, (snapshot) => {
             console.log("LISTENER ACTIVE")
-            const newMessages = [...messages]; // Create a copy of the current messages
-            snapshot.forEach((change) => {
-              console.log("CHANGES OBSERVED")
-              newMessages.push(change.data());
-
-            });
+ // Create a copy of the current messages
+            
+            const newMessages = snapshot.docs.map(doc => doc.data());
             setMessages(newMessages);
           });
     
@@ -76,14 +82,31 @@ const JiltdChat = ({ client_ID, match_ID }) => {
 
 
   // return (<FlatList></FlatList>)
-  return (<View>
-    <FlashButton pressFunc={sendMessage} text={"srs"} ></FlashButton>
+  return (
+    <KeyboardAvoidingView
+    style={{flex:1}}
+    behavior="padding"
+  >
+  <View style = {{flex:1, flexDirection: "column"}}>
+    <View style = {{flex: .25}}><FlashButton pressFunc={sendMessage} text={"x"} ></FlashButton>
+    <FlashButton pressFunc={sendMessage} text={"x"} ></FlashButton></View>
 
-<FlatList data = {messages} renderItem={({item}) => (<Text> {item.millisecond} </Text>)}>
+<View style = {[{ flex: 1}]}><FlatList data = {messages} inverted = {true} renderItem={({item}) => (<View style = {{justifyContent:"flex-end"}} key={item.key}><Text style = {{textAlign: "right", color:"black"}}> {item.millisecond}{item.text} </Text></View>)}/></View>
 
+<View style = {{flex: .40}}>
+<TextInput
+                placeholder = "say something"
+                placeholderTextColor = "white"
+                value = {sampleSend}
+                onChangeText ={text => setSample(text)}
+                style={styles.input}
+            />
   
-</FlatList>
+  <FlashButton pressFunc={testInput} text={"x"} ></FlashButton>
 
-  </View>)
+</View>
+
+  </View>
+  </KeyboardAvoidingView>)
 }
 export default JiltdChat
